bc2cdf2788ae275dd065dc17e7b0d1492884ef55
unknown <nutakki.shivaramakrishna99@gmail.com>
2021-12-15 22:02:58 -0500

    first commit

---
+++ FormProcessing/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using FormProcessing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormProcessing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormProcess formProcess)
+        {
+            FormProcess formProcessResult = new FormProcess();
+            formProcessResult.Result = formProcess.Months.AddDays(formProcess.Days).ToString("dd/MM/yyyy");
+            return View(formProcessResult);
+        }
+    }
+}
---
+++ FormProcessing/Models/FormProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormProcessing.Models
+{
+    public class FormProcess
+    {
+        public int Days { get; set; }
+
+        public DateTime Months { get; set; }
+
+        public string Result { get; set; }
+    }
+}
